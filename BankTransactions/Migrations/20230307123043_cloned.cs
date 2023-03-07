7826979a83423060528bd4aa9ff901d0b3b4a435
Hirushan-N <nadeeshhirushan@gmail.com>
2023-03-07 18:04:19 +0530

    cloned and rebuild db

--- BankTransactions/Migrations/20230307123043_cloned.cs
+++ BankTransactions/Migrations/20230307123043_cloned.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace BankTransactions.Migrations
 {
-    public partial class InitialCreate : Migration
+    public partial class cloned : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -15,11 +15,11 @@ namespace BankTransactions.Migrations
                 {
                     TransactionId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    AccountNumber = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    AccountNumber = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
                     BeneficiaryName = table.Column<string>(type: "nvarchar(100)", nullable: false),
                     BankName = table.Column<string>(type: "nvarchar(100)", nullable: false),
-                    SWIFTCode = table.Column<string>(type: "nvarchar(11)", nullable: false),
-                    Ammount = table.Column<int>(type: "int", nullable: false),
+                    SWIFTCode = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
                     Date = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
                 constraints: table =>
